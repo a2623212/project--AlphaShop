f9a9205d917a6e2301c38a6a9f640b8d431d9355
a2623212@gmail.com <A5GjZK7w>
2022-01-18 00:38:57 +0800

    feat: add js in setpper control and style of destop in form

--- main.js
+++ main.js
@@ -20,20 +20,51 @@ const itemsPanel = document.querySelector('.items-panel')
 const deliveryPanel = document.querySelector('.fee-panel__delivery--fee')
 const totalFeePanel = document.querySelector('.fee-panel__total--fee')
 const stepControl = document.querySelector('.stepper-container')
-const steps = document.querySelectorAll('step')
+const steps = document.querySelectorAll('.step')
+const formParts = document.querySelectorAll('.form-part')
 const btnControl = document.querySelector('#btn-control')
 const btnPrevious = document.querySelector('.btn-previous')
 const btnNext = document.querySelector('.btn-next')
+const btnDone = document.querySelector('.btn-done')
 const deliveryMethods = document.querySelector('.form-part__delivery')
 
-
-let activeCard = 0
 let step = 0
 let deliFee = 0
 let totalFee = 0
 let allItemsFee = 0
 
 // function 
+  // function to control steps and form
+function onBtnControlClick(event) {
+  event.preventDefault()
+  const target = event.target
+  const nowStep = steps[step]
+  if (target.className.includes('btn-next') && target.innerHTML === '下一步'){
+    const nextStep = steps[step + 1]
+    nowStep.classList.remove('active')
+    nowStep.classList.add('checked')
+    nextStep.classList.add('active')
+    formParts[step].classList.toggle('d-none')
+    formParts[step + 1].classList.toggle('d-none')
+    step += 1
+  } else if (target.className.includes('btn-previous')) {
+    const prevStep = steps[ step - 1]
+    nowStep.classList.remove('active')
+    prevStep.classList.remove('checked')
+    prevStep.classList.add('active')
+    formParts[step].classList.toggle('d-none')
+    formParts[step - 1].classList.toggle('d-none')
+    step -= 1
+  }
+  setBtnStyle()
+} 
+
+  // function to control the button style
+  function setBtnStyle() {
+    if 
+  }
+
+  // function to render Part of Cart
 function renderCartItem (array) {
   itemsPanel.innerHTML = ''
   array.forEach((product) => {
@@ -74,7 +105,7 @@ function renderCartItem (array) {
     
   })
 }
-
+  // function to comput the total fee
 function computeTotleFee (array) {
   allItemsFee = 0
 
@@ -87,43 +118,43 @@ function computeTotleFee (array) {
   
 
 }
-
-// EventListener on the quantity Change
-itemsPanel.addEventListener('click', function(event){
-  const productID = Number(event.target.dataset.id)
+  // function to control the items quantity
+function onItemsQuantityBtnClicked(event) {
+   const productID = Number(event.target.dataset.id)
   const quantityContro = event.target.className
 
   const addItem = toBuyProducts.find((product) => product.id === productID)
 
   if (quantityContro.includes('plus')) {
     addItem.quantity += 1
-    renderCartItem (toBuyProducts)
-    computeTotleFee(toBuyProducts)
   } else if (quantityContro.includes('minus')) {
     if(!addItem.quantity){
       return
     }
     addItem.quantity -= 1
-    renderCartItem (toBuyProducts)
-    computeTotleFee(toBuyProducts)
   }
-  
-})
-
-deliveryMethods.addEventListener('click', function (event){
+  renderCartItem (toBuyProducts)
+  computeTotleFee(toBuyProducts)
+}
+  // function to control the delivery fee
+function onDeliveryMethodsRadio(event) {
   const eventName = event.target.tagName.toLowerCase()
   deliFee = Number(event.target.value)
   if (eventName !== 'input') {
     return
   }
   if (deliFee > 0) {
-    deliveryPanel.innerHTML = deliFee
+    deliveryPanel.innerHTML = `$${deliFee}`
   } else {
     deliveryPanel.innerHTML = '免費'
   }
   computeTotleFee(toBuyProducts)
-  
-} )
+}
 
+// EventListener on the quantity Change
+
+itemsPanel.addEventListener('click', onItemsQuantityBtnClicked)
+deliveryMethods.addEventListener('click', onDeliveryMethodsRadio)
+btnControl.addEventListener('click', onBtnControlClick )
 // render here
 renderCartItem (toBuyProducts)
